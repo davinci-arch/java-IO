213312a29bccc60a7411b8ed18029b154a1f686f
parfesa.a15@gmail.com <parfesa.a15@gmail.com>
2023-08-20 19:28:56 +0300

    Recurive travel of directories

--- src/main/java/org/example/NioClasses.java
+++ src/main/java/org/example/NioClasses.java
@@ -2,27 +2,28 @@ package org.example;
 
 import lombok.SneakyThrows;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.channels.SeekableByteChannel;
 import java.nio.file.*;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.regex.Pattern;
 
-import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.WRITE;
 
 public class NioClasses {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        randomAccessFiles("Hello, i was here!\n");
+        Path from = Paths.get("D:/repositories/java-IO");
+        Path to = Paths.get("C:\\Users\\User\\Desktop\\to");
 
+
+        copyAllFilesToDirectory(".java$", from, to);
 
     }
 
@@ -70,10 +71,62 @@ public class NioClasses {
             e.printStackTrace();
         }
 
-
-
-
     }
 
 
+    public static void copyAllFilesToDirectory(String pattern, Path from, Path to) throws IOException {
+
+        List<Path> files = new ArrayList<>();
+
+        if (Files.exists(to) && Files.isDirectory(to)) {
+
+            Files.walkFileTree(from, new FileVisitorEx(pattern, files));
+
+            for (Path file : files) {
+                Files.copy(file, to.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+
+            }
+
+        }
+    }
+
+}
+
+class FileVisitorEx extends SimpleFileVisitor<Path> {
+
+    private final String pattern;
+    private final List<Path> paths;
+
+    public FileVisitorEx(String pattern, List<Path> paths) {
+        this.pattern = pattern;
+        this.paths = paths;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return super.preVisitDirectory(dir, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+        Pattern pattern1 = Pattern.compile(pattern);
+
+        if (pattern1.matcher(file.getFileName().toString()).find()) {
+
+            paths.add(file);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return super.visitFileFailed(file, exc);
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return super.postVisitDirectory(dir, exc);
+    }
 }
